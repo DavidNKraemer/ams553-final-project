224d33df4d002d2aafb893cdcc50b95c38a93aaf
David Kraemer <david.kraemer@stonybrook.edu>
2018-12-02 00:53:16 -0500

    Updates to the cross entropy implementation to make it more readable, with some minor structural modifications.

--- src/cross_entropy.py
+++ src/cross_entropy.py
@@ -1,32 +1,29 @@
 from math import ceil
 
-class Distribution:
-    def __init__(self, density, state_space):
-        self.density = density
-        self.state_space = state_space
-
-    def sample(self, v):
-        pass
-
-    def pdf(self, v, state):
-        return self.density(v, state)
-
-
 
 class CrossEntropy:
-    def __init__(self, f, n, reward, rho):
-        self.f = f
-        self.n = n
-        self.reward = reward
-        self.rho = rho # rho must be nonzero
+    """
 
-    def maximize(self, v0):
-        v = v0
-        for i in range(100):
-            samples = [self.f.sample(v) for _ in range(self.n)]
-            rewards = [self.reward(x) for x in samples]
-            rewards.sort()
-            gamma = rewards[self.n - int(ceil(self.rho * self.n))]
-            v = self.compute_argmax(gamma, samples)
+    """
 
-        return v
+    def __init__(self, state_space, sample_size, quantile, iterations=100):
+        self.state_space = state_space
+        self.sample_size = sample_size
+        self.quantile = quantile  # quantile must be nonzero
+        self.iterations = iterations
+
+    def minimize(self):
+        parameters = self.state_space.get_parameters()
+        for _ in range(self.iterations):
+            samples = [self.state_space.generate_state() for _ in range(self.sample_size)]
+
+            # compute the scores associated with the samples and put them in descending (low = good) order
+            scores = [self.state_space.cost(state) for state in samples]
+            scores.sort(reverse=True)
+
+            # find the upper (1-self.quantile)% threshold of the scores
+            threshold = scores[self.sample_size - int(ceil(self.quantile * self.sample_size))]
+
+            # update the parameters of the state space for the next round
+            parameters = self.state_space.estimate_parameters(threshold, samples, scores)
+            self.state_space.set_parameters(parameters)
